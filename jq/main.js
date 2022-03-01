8951a36758fa8f340ac74d79c3bfce2840e2004a
Moreno27 <alejanmm2000@gmail.com>
2022-03-01 18:54:18 +0100

    animaciones menu

---
+++ jq/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $("#hamburguesa").click(function (e) {
+        e.preventDefault();
+
+        if ($("#menu-toggle").css("left") !== "0px") {
+
+            $("#menu-toggle").animate({
+                left: "0rem"
+            }, "fast");
+
+            $("html, body").css({
+                'overflow': 'hidden',
+            })
+        } else {
+
+            menuPlegado()
+        }
+    });
+});
+
+/**
+ * Animacion que realiza lka recogida del menu desplegable,
+ * ocultandolo con un slide hacia la izquierda
+ */
+function menuPlegado() {
+
+    $("#menu-toggle").animate({
+        left: "-20rem"
+    }, "fast");
+
+    $("html, body").css({
+        'overflow': 'visible',
+    })
+}
